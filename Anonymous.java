09f48f5d021645621400b1ac38e6f87e8e4a0e7d
OptimusPrimeN1 <59879376+OptimusPrimeN1@users.noreply.github.com>
2020-01-27 23:05:19 +0200

    Create Anonymous.java

---
+++ Anonymous.java
@@ -0,0 +1,25 @@
+package fmi.informatics.nestedclasses;
+
+import fmi.informatics.interfaces.Adaptable;
+
+public class Anonymous implements Adaptable {
+
+	@Override
+	public void study() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void think() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
